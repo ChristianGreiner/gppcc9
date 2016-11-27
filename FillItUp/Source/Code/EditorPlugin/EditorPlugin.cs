bd972565aa6c1420417b5a299ff66bebbd6f488b
Christian Greiner <christian.greiner97@gmx.de>
2016-11-27 21:58:59 +0100

    Initial Commit

---
+++ FillItUp/Source/Code/EditorPlugin/EditorPlugin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Duality.Editor;
+
+namespace FillItUp.Editor
+{
+	/// <summary>
+	/// Defines a Duality editor plugin.
+	/// </summary>
+    public class FillItUpEditorPlugin : EditorPlugin
+	{
+		public override string Id
+		{
+			get { return "FillItUpEditorPlugin"; }
+		}
+	}
+}
